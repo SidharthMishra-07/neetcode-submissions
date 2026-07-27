cc918ec58fde42fbe0afde16868fe69839465f1a
Sidharth Mishra <sidharth1604@gmail.com>
2026-07-28 00:22:44 +0530

    Add: decode-ways - submission-3

---
+++ Data Structures & Algorithms/decode-ways/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        if not s or s[0] == '0':
+            return 0
+
+        dp = [0] * (len(s)+1)
+        dp[0] = 1       
+        for i in range(1, len(s)+1):
+            if s[i-1] != '0':     # initially s[0], dp[1] maps to s[0] because, state is number of ways to decode first 1 character of str
+                dp[i] += dp[i-1]
+            if i>=2 and 10 <= int(s[i-2:i]) <= 26:
+                dp[i] += dp[i-2]
+
+        return dp[-1] 
