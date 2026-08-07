743e56535e3482ed53a9e2dc7b50954524dec4eb
Sidharth Mishra <sidharth1604@gmail.com>
2026-08-07 12:07:21 +0530

    Add: coin-change-ii - submission-1

---
+++ Data Structures & Algorithms/coin-change-ii/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        #State: memo[i][j] = number of ways to make j amount using coins[0..i]
+        n = len(coins)
+        memo = [[-1] * (amount+1) for _ in range(n+1)]
+
+        def dfs(i, j):
+            if j == 0:
+                return 1
+            if i>=n or j<0:
+                return 0
+            if memo[i][j] == -1:
+                memo[i][j] = dfs(i, j-coins[i]) + dfs(i+1, j)
+            return memo[i][j]
+        
+        return dfs(0, amount)
