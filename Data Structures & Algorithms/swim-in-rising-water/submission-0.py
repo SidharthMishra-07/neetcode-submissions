2582ca73ad532f008e4e6a170f57054f084e7308
Sidharth Mishra <sidharth1604@gmail.com>
2026-09-16 10:01:39 +0530

    Add: swim-in-rising-water - submission-0

---
+++ Data Structures & Algorithms/swim-in-rising-water/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def swimInWater(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+        res = max(max(row) for row in grid)
+
+        def dfs(r, c, t, visited):
+            if r not in range(rows) or c not in range(cols) or (r,c) in visited or grid[r][c] > t:
+                return False
+            if r == rows-1 and c == cols-1:
+                return True
+
+            visited.add((r,c))
+            return (dfs(r + 1, c, t, visited) or
+                    dfs(r - 1, c, t, visited) or
+                    dfs(r, c + 1, t, visited) or
+                    dfs(r, c - 1, t, visited))
+
+        t = grid[0][0]
+        while t<=res:
+            visited = set()
+            if dfs(0, 0, t, visited):
+                return t
+            t+=1
+
+        return res
